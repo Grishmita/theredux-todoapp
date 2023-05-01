49737835afdb37ccd7739f183720d789b38c6348
Grishmita <grishmitapandey@gmail.com>
2023-05-01 21:56:19 +0530

    second  commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Todos from './components/Todos';
 function App() {
   return (
   <>
-  <marquee style={{backgroundColor: '#fff', padding :'7px'}}>REDUX TODO APP</marquee>
+  <p style={{backgroundColor: '#fff', padding :'7px'}}>REDUX TODO APP</p>
     <div className="App w-6/12 my-24 mx-auto max-md:w-9/12 my-16 max-sm:w-11/12 my-10">
       <Todos />
     </div>
